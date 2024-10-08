56d68d3e187b49c18b6dc28f5ce3e25810c62ff5
banjahman <banjahman@gmail.com>
2024-10-08 10:37:34 -0400

    Update test.viz.js

--- test.viz.js
+++ test.viz.js
@@ -19,32 +19,7 @@ looker.plugins.visualizations.add({
   // Set up the initial state of the visualization
   create: function(element, config) {
 
-    // Insert a <style> tag with some styles we'll use later.
-    element.innerHTML = `
-      <style>
-        .hello-world-vis {
-          /* Vertical centering */
-          height: 100%;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          text-align: center;
-        }
-        .hello-world-text-large {
-          font-size: 72px;
-        }
-        .hello-world-text-small {
-          font-size: 18px;
-        }
-      </style>
-    `;
-
-    // Create a container element to let us center the text.
-    var container = element.appendChild(document.createElement("div"));
-    container.className = "hello-world-vis";
-
-    // Create an element to contain the text.
-    this._textElement = container.appendChild(document.createElement("div"));
+    element.innerHTML = "<h1>Ready to render!</h1>";
 
   },
   // Render in response to the data or settings changing
